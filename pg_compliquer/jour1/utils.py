5320ecb28fa4154c38670cdf4521fc40e633f73f
skaleurleboss <andrea.skaleur@gmail.com>
2025-10-05 11:22:38 +0200

    ajout d'un parser

--- pg_compliquer/jour1/utils.py
+++ pg_compliquer/jour1/utils.py
@@ -1,9 +1,12 @@
 import BeauMenu
 import pathlib
 from datetime import datetime
+from urllib.request import urlopen
+from html.parser import HTMLParser
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 
-def counterword() :
-    ph=BeauMenu.belle_input("entrer une phrase : ")
+def counterword(ph) :
     phl=ph.split(" ")
     dictio={}
     list_mot_deja_vu=[]
@@ -18,50 +21,142 @@ def counterword() :
             dictio[w] = 1
     dic_tuple=dictio.items()
     tuple_sorted=sorted(dic_tuple,key=lambda x:x[1], reverse=True)
-    for msg in tuple_sorted :
-        BeauMenu.beau_menu(msg)
+    return tuple_sorted
 
-def no_doublon() :
-    ph=BeauMenu.belle_input("entrer une phrase : ")
+def no_doublon(ph) :
     ph1=ph.split(" ")
     ph2=set(ph1)
-    print(ph2)
     return ph2
 
-def tree_first_word() :
-    ph=BeauMenu.belle_input("entrer une phrase : ")
+def tree_first_word(ph) :
     ph1=ph.split(" ")
     list_mot=ph1[:3]
     ph1.reverse()
     list_mot2=ph1[:3]
     list_mot.extend(list_mot2)
-    print(list_mot)
+    return list_mot
 
-def write_word() :
-    ph=BeauMenu.belle_input("entrer une phrase : ")
+def write_word(ph) :
     with open("txt.txt", "w", encoding="utf-8") as f:
         f.write(ph)
 
 def read_word() :
     with open("txt.txt", "r", encoding="utf-8") as f:
         contenu = f.read()
-    print(contenu)
+    return contenu
 
 def lister_fichier():
     ph=BeauMenu.belle_input("entrer un chemin")
     for f in pathlib.Path(ph).rglob("*.txt"):
-        print(f)
+        return f
 
-def meta_donnee():
-    p = pathlib.Path("txt.txt")
+def meta_donnee(pa):
+    p = pathlib.Path(pa)
     st = p.stat()
 
-    print("Taille:", st.st_size, "octets")
-    print("Extension:", p.suffix)
-    print("Nom:", p.stem)
-    print("Modifié:", datetime.fromtimestamp(st.st_mtime))
+    file={"taille":st.st_size,"extension":p.suffix,"nom":p.stem,"modification":datetime.fromtimestamp(st.st_mtime)}
+    return file
 
-write_word()
-read_word()
-lister_fichier()
-meta_donnee()
+def scan_dir(pa, glob):
+    pa = pathlib.Path()
+    dir=[]
+    for f in pa(".").rglob(glob):
+        dir.append(f)
+    return f
+
+def file_weight(operator,sort_taille,dir) :
+    file=meta_donnee(dir)
+    taille=file[taille]
+    final_file=[]
+    for file in dir :
+        file=meta_donnee(dir)
+        taille=file[taille]
+        if operator == "<=" :
+            if taille <= sort_taille :
+                final_file.append(file)
+        if operator == ">=" :
+            if taille >= sort_taille :
+                final_file.append(file)
+        if operator == "==" :
+            if taille == sort_taille :
+                final_file.append(file)
+    return final_file
+
+class PageParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.in_title = False
+        self.in_h1 = False
+        self.title = None
+        self.links = []
+        self.h1 = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "title":
+            self.in_title = True
+        if tag == "h1":
+            self.in_h1 = True
+        if tag == "a":
+            hrefs = [v for (k, v) in attrs if k == "href"]
+            self.links.extend(hrefs)
+            self.links = list(set(self.links))
+
+    def handle_endtag(self, tag):
+        if tag == "h1":
+            self.in_h1 = False
+        if tag == "title":
+            self.in_title = False
+
+    def handle_data(self, data):
+        if self.in_h1:
+            self.h1.append(data)
+        if self.in_title:
+            self.title = (self.title or "") + data.strip()
+
+    def countInternalLink(self):
+        counter = 0 
+        for l in self.links:
+            if l[0]=="/":
+                counter = counter + 1
+        return counter
+
+    
+def fetch_page(url: str) -> dict:
+    with urlopen(url, timeout=5) as response:
+        encoding = response.headers.get_content_charset() or "utf-8"
+        html = response.read().decode(encoding)
+
+    parser = PageParser()
+    parser.feed(html)
+
+    #sdir = slugify(url)
+    # Create Directory
+    #os.makedirs(sdir)
+
+    return {
+        "h1": parser.h1,
+        "url": url,
+        "title": parser.title,
+        "links": parser.links,
+        "count_internal": parser.countInternalLink()
+    }
+
+
+
+def check_url(url: str) -> bool:
+    try:
+        # on envoie une requête HEAD pour limiter la charge
+        req = Request(url, method="HEAD")
+        with urlopen(req, timeout=5) as resp:
+            code = resp.getcode()
+            # considère "ok" si code 200–299
+            return 200 <= code < 300
+    except HTTPError as e:
+        print(f"HTTP error: {e.code} for {url}")
+    except URLError as e:
+        print(f"Connection error: {e.reason} for {url}")
+    except Exception as e:
+        print(f"Other error: {e}")
+    return False
+
+
